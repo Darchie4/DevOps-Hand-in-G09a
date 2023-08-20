676fdbced83f49a0c161639cabcc2fd70dee8292
OthelloEngineer <jolee18@student.sdu.dk>
2023-08-20 07:44:00 +0000

    feat: added healthz test

--- frontend/main.go
+++ frontend/main.go
@@ -29,11 +29,42 @@ type newFortune struct {
 var myClient = &http.Client{Timeout: 10 * time.Second}
 
 func HealthzHandler(w http.ResponseWriter, r *http.Request) {
-	w.WriteHeader(http.StatusOK)
-	_, err := io.WriteString(w, "healthy")
+	
+	status := false
+	resp, err := myClient.Get(fmt.Sprintf("http://%s:%s/fortunes", BACKEND_DNS, BACKEND_PORT)) //nolint:all
+	if err != nil {
+		log.Fatalln(err)
+		fmt.Fprint(w, err)
+		return
+	}
+	fortunes := new([]fortune)
+	err = json.NewDecoder(resp.Body).Decode(fortunes)
 	if err != nil {
 		log.Fatal(err)
 	}
+
+	if len(fortunes)  != 0 {
+		status = true
+		fmt.Println("test passed")
+		for _, val in for range fortunes {
+			fmt.Println(val)
+		}
+	}
+	
+	if status == true {
+		w.WriteHeader(http.StatusOK)
+		_, err := io.WriteString(w, "healthy")
+		if err != nil {
+			log.Fatal(err)
+		}
+		return
+	}
+	w.WriteHeader(http.StatusServiceUnavailable)
+	_, err := io.WriteString(w, "not so healthy")
+	if err != nil {
+		log.Fatal(err)
+	}
+
 }
 
 func main() {
