b4613920df10d457dffcb7f3406e761c610e2abb
Mads Würtz Pedersen <peder21@student.sdu.dk>
2023-08-20 07:52:32 +0000

    feat(backend-test): finished work on /healtz test

--- backend/main_test.go
+++ backend/main_test.go
@@ -7,8 +7,22 @@ import (
 )
 
 func TestHealthz(t *testing.T) {
-    req, err := http.NewRequest("GET", "/healthz", nil)
+    _, err := http.NewRequest("GET", "/healthz", nil)
     if err != nil {
         t.Fatal(err)
+    }
+
+    rr := httptest.NewRecorder()
+
+    status := rr.Code;
+
+    if status != http.StatusOK {
+        t.Errorf("handler returned wrong status code: got %v want %v", status, http.StatusOK)
+    }
+
+    expected := "healthy"
+    if rr.Body.String() != expected {
+        t.Errorf("handler returned unexpected body: got %v want %v",
+            rr.Body.String(), expected)
     }
 }
