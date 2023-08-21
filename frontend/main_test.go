000001104c805fe614584cab4748e4e24c5ec350
Mads Würtz Pedersen <peder21@student.sdu.dk>
2023-08-21 16:23:48 +0200

    feat: improved health check #9

--- frontend/main_test.go
+++ frontend/main_test.go
@@ -1,16 +1,29 @@
 package main
 
 import (
+	"bytes"
+	"encoding/json"
     "net/http"
-    "net/http/httptest"
     "testing"
 )
 
-func TestHealthz(t *testing.T) {
+// main_test.go
+func TestHealthMarshaller(t *testing.T) {
+	data := []byte(`[
+        {"id":"2","message":"The measure of time to your next goal is the measure of your discipline."},
+        {"id":"3","message":"The only way to do well is to do better each day."},
+        {"id":"4","message":"It ain't over till it's EOF."},
+        {"id":"1","message":"A new voyage will fill your life with untold memories."}
+    ]`)
 
-    // Create a request to pass to our handler. We don't have any query parameters for now, so we'll
-    // pass 'nil' as the third parameter.
-    req, err := http.NewRequest("GET", "/healthz", nil)
+	reader := bytes.NewReader(data)
+	status := HealthMarshaller(reader)
+
+	expected := true
+	if status != expected {
+		t.Errorf("Marshalling did not result in correct fortunes")
+	}
+}
     if err != nil {
         t.Fatal(err)
     }
